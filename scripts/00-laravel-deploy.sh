e5cb5beb7813452c47aa027f26bea68a0faa2d88
faizathaya19 <faizathayaramadhan@gmail.com>
2023-11-06 23:21:08 +0700

    p

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -6,6 +6,8 @@ composer install --no-dev --working-dir=/var/www/html
 echo "Caching config..."
 php artisan config:cache
 
+php artisan key:generate --show
+
 echo "Caching routes..."
 php artisan route:cache
 
