670822611c7bd8c04e7be13e4bfa0076d109d16b
faizathaya19 <faizathayaramadhan@gmail.com>
2023-11-06 23:00:55 +0700

    p

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,112 +1,80 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
-        <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet">
-        <style>
-            /*Form fields*/
-            .dataTables_wrapper select,
-            .dataTables_wrapper .dataTables_filter input {
-                color: #4a5568; 			/*text-gray-700*/
-                padding-left: 1rem; 		/*pl-4*/
-                padding-right: 1rem; 		/*pl-4*/
-                padding-top: .5rem; 		/*pl-2*/
-                padding-bottom: .5rem; 		/*pl-2*/
-                line-height: 1.25; 			/*leading-tight*/
-                border-width: 2px; 			/*border-2*/
-                border-radius: .25rem; 		
-                border-color: #edf2f7; 		/*border-gray-200*/
-                background-color: #edf2f7; 	/*bg-gray-200*/
-            }
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-            /*Row Hover*/
-            table.dataTable.hover tbody tr:hover, table.dataTable.display tbody tr:hover {
-                background-color: #ebf4ff;	/*bg-indigo-100*/
-            }
-            
-            /*Pagination Buttons*/
-            .dataTables_wrapper .dataTables_paginate .paginate_button		{
-                font-weight: 700;				/*font-bold*/
-                border-radius: .25rem;			/*rounded*/
-                border: 1px solid transparent;	/*border border-transparent*/
-            }
-            
-            /*Pagination Buttons - Current selected */
-            .dataTables_wrapper .dataTables_paginate .paginate_button.current	{
-                color: #fff !important;				/*text-white*/
-                box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06); 	/*shadow*/
-                font-weight: 700;					/*font-bold*/
-                border-radius: .25rem;				/*rounded*/
-                background: #667eea !important;		/*bg-indigo-500*/
-                border: 1px solid transparent;		/*border border-transparent*/
-            }
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-            /*Pagination Buttons - Hover */
-            .dataTables_wrapper .dataTables_paginate .paginate_button:hover		{
-                color: #fff !important;				/*text-white*/
-                box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06);	 /*shadow*/
-                font-weight: 700;					/*font-bold*/
-                border-radius: .25rem;				/*rounded*/
-                background: #667eea !important;		/*bg-indigo-500*/
-                border: 1px solid transparent;		/*border border-transparent*/
-            }
-            
-            /*Add padding to bottom border */
-            table.dataTable.no-footer {
-                border-bottom: 1px solid #e2e8f0;	/*border-b-1 border-gray-300*/
-                margin-top: 0.75em;
-                margin-bottom: 0.75em;
-            }
-            
-            /*Change colour of responsive icon*/
-            table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before, table.dataTable.dtr-inline.collapsed>tbody>tr>th:first-child:before {
-                background-color: #667eea !important; /*bg-indigo-500*/
-            }
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-        </style>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-        @livewireStyles
+                    </ul>
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-        <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
                 </div>
-            </header>
+            </div>
+        </nav>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-
-        @stack('modals')
-
-        @livewireScripts
-
-        {{ $script ?? '' }}
-    </body>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
 </html>
