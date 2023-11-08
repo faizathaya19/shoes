2c9321fc4f77d4ce28483328c9a75af61e8de40b
Faiz Athaya Ramadhan <76005226+faizathaya19@users.noreply.github.com>
2023-11-08 08:37:32 +0700

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Running composer"
-composer global require hirak/prestissimo:^0.3.10
+composer require hirak/prestissimo:^0.3.10 --dev --working-dir=/var/www/html
 
-# Ensure the required versions are installed globally
-composer require composer-plugin-api:^1.0.0
-composer require php:^8.2.7
+# Ensure the required versions are installed locally
+composer require composer-plugin-api:^1.0.0 --dev --working-dir=/var/www/html
+composer require php:^8.2.7 --dev --working-dir=/var/www/html
 
 composer install --no-dev --working-dir=/var/www/html
 
