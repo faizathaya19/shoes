49d155557c06385fa715ac40e837b4de11da1184
Faiz Athaya Ramadhan <76005226+faizathaya19@users.noreply.github.com>
2023-11-08 08:08:45 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,9 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create
-            DB::statement("alter table child_tbl inherit parent_tbl");
-        ('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
