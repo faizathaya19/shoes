17c378449d93aa7c9dd4800007876ab10a73911b
Faiz Athaya Ramadhan <76005226+faizathaya19@users.noreply.github.com>
2023-11-08 08:12:47 +0700

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-echo "Running composer"
-cp /etc/secrets/.env .env
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
 
-echo "Clearing caches..."
-php artisan optimize:clear
+echo "Running composer"
+composer global require hirak/prestissimo:^0.3.10
+
+# Ensure the required versions are installed globally
+composer require composer-plugin-api:^1.0.0 --global
+
+# Assuming you have PHP version 8.2.7 globally installed
+composer require php:^8.2.7 --global
+
+composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
 php artisan config:cache
@@ -15,5 +19,3 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
-
-echo "done deploying"
